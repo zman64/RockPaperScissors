52815d9be25bcd3de73846df48ebe7ae26e5a179
Zachary Kahlig <zkahlig64@gmail.com>
2022-04-13 16:13:06 -0400

    Add function that plays a single round
    Function takes 2 parameters, players and computer selection.
    function is case-insensitive

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ function computerPlay(){
     const randomChoiceIndex = Math.floor(Math.random() * 3)
     return choices[randomChoiceIndex];
 }
+
+function singleRound(playerSelection, computerSelection){
+    let playerSelection1 = playerSelection.toLowerCase();
+    if(playerSelection1 === "rock" && computerSelection === "Paper"){
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection1 === "rock" && computerSelection === "Scissors"){
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection1 === "paper" && computerSelection === "Rock"){
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection1 === "paper" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection1 === "scissors" && computerSelection === "Rock"){
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection1 === "scissors" && computerSelection === "Paper"){
+        return "You Win! Scissors beats Paper"
+    }  else {
+        return "It's a tie!"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(singleRound(playerSelection, computerSelection))
