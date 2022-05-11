6edd53bf78e9d3108d528e8d5930a17b85d101a7
Zachary Kahlig <zkahlig64@gmail.com>
2022-05-10 22:03:15 -0400

    Add Beginning UI 3 Buttons and Display
    Add three User buttons (Rock, Paper, Scissors)
    Display text with results each round
    Need running score (best of 5)
    Need to keep buttons from moving...

--- index.js
+++ index.js
@@ -5,79 +5,89 @@ function computerPlay(){
     const randomChoiceIndex = Math.floor(Math.random() * 3)
     return choices[randomChoiceIndex];
 }
-
+const container = document.getElementById('container')
 //return a number based on who wins
-function singleRound(playerSelection, computerSelection){
-    // make player choice input case-insensitive
-    let playerSelection1 = playerSelection.toLowerCase();
-     while(playerSelection1 !== 'rock' && playerSelection1 !== 'scissors' && playerSelection1 !== 'paper'){
-        
-        playerSelection1 = prompt("Not Valid Choice and please try again");
-        playerSelection1 = playerSelection1.toLowerCase();
+function playRound(e){
+
+    let playerSelection = e.target.value
+    let computerSelection = computerPlay();
+    let display = document.createElement('div');
+    
+    if(playerSelection === "Rock" && computerSelection === "Paper"){
+
+        display.textContent = "You Lose! Paper beats Rock"
+
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors"){
+        display.textContent = "You Win! Rock beats Scissors"
+       
+    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
+
+        display.textContent = "You Win! Paper beats Rock"
+
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+
+        display.textContent = "You Lose! Scissors beats Paper"
+
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+
+        display.textContent = "You Lose! Rock beats Scissors"
+
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+
+        display.textContent = "You Win! Scissors beats Paper"
+
+    }  else {
+        display.textContent = "It's a tie!"
     }
 
-    console.log(`Your Selection: ${playerSelection1} \nComputer Selection: ${computerSelection}`)
     
-    if(playerSelection1 === "rock" && computerSelection === "Paper"){
-        console.log("You Lose! Paper beats Rock")
-        return 2;
-    } else if (playerSelection1 === "rock" && computerSelection === "Scissors"){
-        console.log("You Win! Rock beats Scissors")
-        return 1;
-    } else if (playerSelection1 === "paper" && computerSelection === "Rock"){
-        console.log("You Win! Paper beats Rock")
-        return 1;
-    } else if (playerSelection1 === "paper" && computerSelection === "Scissors") {
-        
-        console.log("You Lose! Scissors beats Paper")
-        return 2
-    } else if (playerSelection1 === "scissors" && computerSelection === "Rock"){
-        
-        console.log("You Lose! Rock beats Scissors")
-        return 2
-    } else if (playerSelection1 === "scissors" && computerSelection === "Paper"){
-        "You Win! Scissors beats Paper"
-        return 1
-    }  else {
-        console.log("It's a tie!")
-        return 3
-    }
+    //let text = ${text} You chose ${playerSelection} and Computer chose `${computerSelection}.
+    display.classList.add('h-1','text-white','mt-3')
+    container.appendChild(display)
 }
+
+
+
+let buttons = document.getElementsByClassName('btn-lg')
+
+Array.from(buttons).forEach(function(button){
+    button.addEventListener('click', playRound)
+})
 
 // main game function
-function game(){
-    var computerScore = 0;
-    var playerScore = 0;
-    let outcomeResults = "";
-    // for each round of game (best of 5 games)
-    for(let i =0; i < 5; i++){
-        // prompt user for choice
-        let playerSelection = prompt("Let's Play! Choose Rock, Paper, or Scissors")
+// function game(){
+//     var computerScore = 0;
+//     var playerScore = 0;
+//     let outcomeResults = "";
+//     // for each round of game (best of 5 games)
+//     for(let i =0; i < 5; i++){
+//         // prompt user for choice
+//         let playerSelection = prompt("Let's Play! Choose Rock, Paper, or Scissors")
 
-        // create computer choice
-        let computerSelection = computerPlay();
+//         // create computer choice
+//         let computerSelection = computerPlay();
 
-        // handle a single Round and return who won
-        let answer = singleRound(playerSelection, computerSelection, computerScore, playerScore);
+//         // handle a single Round and return who won
+//         let answer = singleRound(playerSelection, computerSelection, computerScore, playerScore);
 
-        // increment score based on who won
-        if (answer === 1){
-            playerScore++;
-        } else if(answer === 2){
-            computerScore++;
-        } else {
+//         // increment score based on who won
+//         if (answer === 1){
+//             playerScore++;
+//         } else if(answer === 2){
+//             computerScore++;
+//         } else {
 
-        }
-    }
-    // log who wins best out of 5 games
-    if(playerScore > computerScore){
-        console.log("You Win best out of 5! Congrats")
-    } else if (playerScore === computerScore){
-        console.log("It's a tie game!!")
-    } else {
-        console.log("You Lose the entire Game!!")
-    }
-}
+//         }
+//     }
+//     // log who wins best out of 5 games
+//     if(playerScore > computerScore){
+//         console.log("You Win best out of 5! Congrats")
+//     } else if (playerScore === computerScore){
+//         console.log("It's a tie game!!")
+//     } else {
+//         console.log("You Lose the entire Game!!")
+//     }
+// }
 
 // const sumArray = function(array) {
 //     let sum = 0;
@@ -89,7 +99,7 @@ function game(){
 // sumArray([2,2,2])
 
 // start game function
-game();
+// game();
 
 // const playerSelection = "rock";
 // const computerSelection = computerPlay();
